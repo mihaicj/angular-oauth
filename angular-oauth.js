60bb9b222aaec7ea6fab04dae20ff3a63a7f69a8
ovidiu <ovidiu.iovan@agilio.eu>
2017-01-05 14:07:38 +0200

    generate token based on grant type and api key

--- angular-oauth.js
+++ angular-oauth.js
@@ -1,152 +1,148 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('mm.oauth', [])
-        .provider('oAuthService', oAuthProvider);
+	angular
+		.module('mm.oauth', [])
+		.provider('oAuthService', oAuthProvider);
 
-    function oAuthProvider() {
-        var config,
-            requiredConfigFields = ['baseUrl', 'clientId', 'clientSecret'],
-            defaultConfigFields = {
-                baseUrl: null,
-                clientId: null,
-                clientSecret: null,
-                grantPath: '/oauth/v2/token',
-                storageKey: 'OAuth-data'
-            };
+	function oAuthProvider() {
+		var config,
+			requiredConfigFields = ['baseUrl', 'clientId', 'clientSecret'],
+			defaultConfigFields = {
+				baseUrl: null,
+				clientId: null,
+				clientSecret: null,
+				grantPath: '/oauth/v2/token',
+				storageKey: 'OAuth-data'
+			};
 
-        this.configure = function (params) {
-            // Check if the config parameters is an object
-            if (!(params instanceof Object)) {
-                throw Error('Invalid argument: `config` must be an `object`.');
-            }
+		this.configure = function (params) {
+			// Check if the config parameters is an object
+			if (!(params instanceof Object)) {
+				throw Error('Invalid argument: `config` must be an `object`.');
+			}
 
-            // Check if the required config fields are set
-            angular.forEach(requiredConfigFields, function (field) {
-                if (!params[field]) {
-                    throw Error('Missing parameter: ' + field);
-                }
-            });
+			// Check if the required config fields are set
+			angular.forEach(requiredConfigFields, function (field) {
+				if (!params[field]) {
+					throw Error('Missing parameter: ' + field);
+				}
+			});
 
-            // Set the config fields
-            config = angular.extend({}, defaultConfigFields, params);
+			// Set the config fields
+			config = angular.extend({}, defaultConfigFields, params);
 
-            config.baseUrl = config.baseUrl.replace(/\/$/, '');
+			config.baseUrl = config.baseUrl.replace(/\/$/, '');
 
-            return config;
-        };
+			return config;
+		};
 
-        this.$inject = ['$q', '$http'];
-        this.$get = function ($q, $http) {
-            return new Oauth($q, $http, config);
-        };
-    }
+		this.$inject = ['$q', '$http'];
+		this.$get = function ($q, $http) {
+			return new Oauth($q, $http, config);
+		};
+	}
 
-    var Oauth = function ($q, $http, config) {
-        return {
-            isAuthenticated: isAuthenticated,
-            generateAccessToken: generateAccessToken,
-            generateAccessTokenWithGrant: generateAccessTokenWithGrant,
-            refreshAccessToken: refreshAccessToken,
-            getOAuthData: getOAuthData,
-            removeAccessToken: removeAccessToken
-        };
+	var Oauth = function ($q, $http, config) {
+		return {
+			isAuthenticated: isAuthenticated,
+			generateAccessToken: generateAccessToken,
+			generateAccessTokenWithGrant: generateAccessTokenWithGrant,
+			refreshAccessToken: refreshAccessToken,
+			getOAuthData: getOAuthData,
+			removeAccessToken: removeAccessToken
+		};
 
-        /**
-         * Requests a new access token using a username and password
-         * @param {string} username
-         * @param {string} password
-         * @returns {promise}
-         */
-        function generateAccessToken(username, password) {
-            var data = {
-                username: username,
-                password: password,
-                grant_type: 'password',
-                client_id: config.clientId,
-                client_secret: config.clientSecret
-            };
+		/**
+		 * Requests a new access token using a username and password
+		 * @param {string} username
+		 * @param {string} password
+		 * @returns {promise}
+		 */
+		function generateAccessToken(username, password) {
+			var data = {
+				username: username,
+				password: password,
+				grant_type: 'password',
+				client_id: config.clientId,
+				client_secret: config.clientSecret
+			};
 
-            return $http.post(config.baseUrl + config.grantPath, data)
-                .then(function (response) {
-                    return storeOAuthData(response.data);
-                });
-        }
+			return $http.post(config.baseUrl + config.grantPath, data)
+				.then(function (response) {
+					return storeOAuthData(response.data);
+				});
+		}
 
-        /**
-         * Requests a new access token using a grant type and api key
-         * @param {string} grantType
-         * @param {string} apiKey
-         * @returns {promise}
-         */
-        function generateAccessTokenWithGrant(grantType, apiKey) {
-            var data = {
-                grant_type: grantType,
-                api_key: apiKey,
-                client_id: config.clientId,
-                client_secret: config.clientSecret
-            };
+		/**
+		 * Requests a new access token using a grant type and api key
+		 * @param {string} grantType
+		 * @param {string} apiKey
+		 * @returns {promise}
+		 */
+		function generateAccessTokenWithGrant(data) {
+			data.client_id = config.clientId;
+			data.client_secret = config.clientSecret;
 
-            return $http.post(config.baseUrl + config.grantPath, data)
-                .then(function (response) {
-                    return storeOAuthData(response.data);
-                });
-        }
+			return $http.post(config.baseUrl + config.grantPath, data)
+				.then(function (response) {
+					return storeOAuthData(response.data);
+				});
+		}
 
-        /**
-         * Request a new access token using the stored refresh token
-         * @returns {promise}
-         */
-        function refreshAccessToken() {
-            var oAuthData = getOAuthData();
+		/**
+		 * Request a new access token using the stored refresh token
+		 * @returns {promise}
+		 */
+		function refreshAccessToken() {
+			var oAuthData = getOAuthData();
 
-            if (!oAuthData || !oAuthData.refresh_token) {
-                return $q.reject();
-            }
+			if (!oAuthData || !oAuthData.refresh_token) {
+				return $q.reject();
+			}
 
-            var	data = {
-                refresh_token: getOAuthData().refresh_token,
-                grant_type: 'refresh_token',
-                client_id: config.clientId,
-                client_secret: config.clientSecret
-            };
+			var data = {
+				refresh_token: getOAuthData().refresh_token,
+				grant_type: 'refresh_token',
+				client_id: config.clientId,
+				client_secret: config.clientSecret
+			};
 
-            return $http.post(config.baseUrl + config.grantPath, data)
-                .then(function (response) {
-                    return storeOAuthData(response.data);
-                });
-        }
+			return $http.post(config.baseUrl + config.grantPath, data)
+				.then(function (response) {
+					return storeOAuthData(response.data);
+				});
+		}
 
-        function isAuthenticated() {
-            var oAuthData = getOAuthData();
+		function isAuthenticated() {
+			var oAuthData = getOAuthData();
 
-            return oAuthData && oAuthData.access_token;
-        }
+			return oAuthData && oAuthData.access_token;
+		}
 
-        function storeOAuthData(data) {
-            data.createdAt = Date.now();
+		function storeOAuthData(data) {
+			data.createdAt = Date.now();
 
-            localStorage.setItem(config.storageKey, JSON.stringify(data));
+			localStorage.setItem(config.storageKey, JSON.stringify(data));
 
-            return data;
-        }
+			return data;
+		}
 
-        function getOAuthData() {
-            var jsonData = localStorage.getItem(config.storageKey);
+		function getOAuthData() {
+			var jsonData = localStorage.getItem(config.storageKey);
 
-            try {
-                return JSON.parse(jsonData);
-            } catch(error) {
-                return {};
-            }
-        }
+			try {
+				return JSON.parse(jsonData);
+			} catch (error) {
+				return {};
+			}
+		}
 
-        /**
-         * Removes access token
-         */
-        function removeAccessToken(){
-            localStorage.removeItem(config.storageKey);
-        }
-    }
+		/**
+		 * Removes access token
+		 */
+		function removeAccessToken() {
+			localStorage.removeItem(config.storageKey);
+		}
+	}
 })();
